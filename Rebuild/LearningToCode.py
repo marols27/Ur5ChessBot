bf673b055393673641af400f8875610d7daa138b
Martin Repvik Olsbø <martinolsbo1@gmail.com>
2024-10-14 04:56:59 -0700

    prepared poseconfig and gui

--- Rebuild/LearningToCode.py
+++ Rebuild/LearningToCode.py
@@ -7,19 +7,10 @@ import chess.engine
 import chess.pgn
 import Settings
 from PoseConfigure import PoseConfigure
-from Rebuild.gui import GUI
+from gui import GUI
 
-# robot = UR5Robot(Settings.TRAVEL_HEIGHT, Settings.HOME, Settings.CONNECTION_IP, Settings.ACCELERATION, Settings.SPEED, Settings.GRIPPER_SPEED, Settings.GRIPPER_FORCE)
-# dgt = DGTBoard(Settings.PORT)
-# board = Board(Settings.START_FEN, Settings.BOARD_FEATURE, Settings.BOARD_SIZE, Settings.SQUARE_SIZE)
-# engine = chess.engine.SimpleEngine.popen_uci(Settings.STOCKFISH_PATH)
-# gameInfo = chess.pgn.Game()
-# capturePos = Settings.CAPTURE_POSE
-# timeout = Settings.TIMEOUT
+
+#pose = PoseConfigure()
+#pose.firstTimeSettup()
+
 gui = GUI()
-
-#game = Game(robot, dgt, board, engine, gameInfo, capturePos, timeout, 10, False)
-
-#game.runGameLoop()
-
-#print(game.gameInfo)
